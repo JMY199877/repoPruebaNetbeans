a3f830ecb3bb548eefb0865474b133a2eeb14a1d
JMY199877 <114702357+JMY199877@users.noreply.github.com>
2022-11-03 22:39:23 +0100

    commit segundo

--- src/org/japo/java/main.java
+++ src/org/japo/java/main.java
@@ -27,7 +27,7 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("");
+        System.out.println("holamundo");
     }
 
 }
